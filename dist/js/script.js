2c9f5a83fabac8ea0411d1f35ca22b2200cbc665
austinmayer10969 <409040630@gms.tku.edu.tw>
2022-06-19 21:39:45 +0800

    add dist files

--- dist/js/script.js
+++ dist/js/script.js
@@ -24,3 +24,23 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle('nav-open');
   });
 });
+
+const sectionHeroEl = document.querySelector('.section-about');
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(sectionHeroEl);
